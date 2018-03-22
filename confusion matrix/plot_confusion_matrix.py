4a17643c8db6186d7c42b59e03cf285abc20854d
oadelso <oseas.adelso@gmail.com>
2018-03-22 15:04:16 -0700

    Update plot_confusion_matrix.py

--- confusion matrix/plot_confusion_matrix.py
+++ confusion matrix/plot_confusion_matrix.py
@@ -1,26 +1,21 @@
+#code obtained from: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
 print(__doc__)
 
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import pandas as pd
 
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
 # import some data to play with
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-class_names = iris.target_names
-
-# Split the data into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-# Run classifier, using a model that is too regularized (C too low) to see
-# the impact on the results
-classifier = svm.SVC(kernel='linear', C=0.01)
-y_pred = classifier.fit(X_train, y_train).predict(X_test)
+y_val = pd.read_csv('outputs.csv')
+y_pred = pd.read_csv('labels.csv')
+class_names = ['Neutral','Happy','Sad','Surprise','Fear','Disgust','Anger','Contempt','None','Uncertain','Non-Face']
 
 
 def plot_confusion_matrix(cm, classes,
@@ -59,7 +54,7 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
 
 # Compute confusion matrix
-cnf_matrix = confusion_matrix(y_test, y_pred)
+cnf_matrix = confusion_matrix(y_pred, y_val)
 np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
